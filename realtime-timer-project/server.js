c4818e2b6dcae2f5b3c4ff96b33aa21b5ad37b61
Jovandi <162522277+MrJov100@users.noreply.github.com>
2025-04-30 08:43:56 +0700

    Update feature: mirroring in Controller

--- realtime-timer-project/server.js
+++ realtime-timer-project/server.js
@@ -10,23 +10,58 @@ app.use("/controller", express.static(__dirname + "/controller"));
 app.use("/viewer", express.static(__dirname + "/viewer"));
 app.use("/public", express.static(__dirname + "/public"));
 
+app.get("/", (req, res) => {
+  res.send("Silakan buka /controller atau /viewer");
+});
+
+// State session aktif
+let currentSession = {
+  title: "",
+  speaker: "",
+  speech: "",
+  message: "",
+  remainingSeconds: 0,
+};
+
 io.on("connection", (socket) => {
   console.log("Client connected");
 
+  // Kirim state saat ini ke controller saat terkoneksi
+  socket.emit("current-session", currentSession);
+
   socket.on("update-timer", (data) => {
+    currentSession.title = data.title;
+    currentSession.speaker = data.speaker;
+    currentSession.speech = data.speech;
+    currentSession.remainingSeconds =
+      parseInt(data.minutes) * 60 + parseInt(data.seconds);
+
     io.emit("update-timer", data);
+    io.emit("current-session", currentSession); // broadcast update ke controller
   });
 
   socket.on("send-message", (message) => {
+    currentSession.message = message;
     io.emit("send-message", message);
+    io.emit("current-session", currentSession);
   });
 
   socket.on("clear-message", () => {
+    currentSession.message = "";
     io.emit("clear-message");
+    io.emit("current-session", currentSession);
   });
 
   socket.on("reset-viewer", () => {
+    currentSession = {
+      title: "",
+      speaker: "",
+      speech: "",
+      message: "",
+      remainingSeconds: 0,
+    };
     io.emit("reset-viewer");
+    io.emit("current-session", currentSession);
   });
 });
 
